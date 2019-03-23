7bfe5c366cb43f1874aa25b30f82341ad51cfc3d
Exorust <chandrahas.aroori@gmail.com>
2019-03-23 06:13:39 +0530

    Todo

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -4,21 +4,31 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class Perceptron(object):
+class Perceptron:
     """docstring for Perceptron."""
-
-    def __init__(self, X,y,t,learning_parameter=0.01):
+    def __init__(self, X,t,total_cycles,learning_parameter=0.01):
         self.X = X
-        self.y = y
+        # self.y = y
         self.t = t
-        # TODO: Make E and del_E arrays
-        self.E=0 #setting inital error to 0
+        self.total_cycles = total_cycles
+        self.E=np.zeros(self.total_cycles) #setting inital error to 0
+        self.del_E = np.zeros((self.total_cycles,3))
         self.w=np.random.rand(3)#Forming the initial value of w which is a set of 3 points between 0 and 1
         self.w_T=self.w.T#Finding the transpose of w
         self.learning_parameter = learning_parameter
-        self.del_E = 0
+
         #Needed?
-        plt.figure(1)
+        # plt.figure(1)
+
+    def epoch(self,count):
+        for i in range(self.X.shape[0]):
+            wTx=np.dot(self.w.T,self.X[i])#Calculating W_transpose*x
+            if wTx*t[i]<0:#Finding those points incorrectly misclassified
+                self.E[count] += -1*wTx*self.t[i]#Calculating total error
+                self.del_E[count] += X[i] * self.t[i]#Calculating delE
+        self.w = self.w + (self.learning_parameter * self.del_E[count]) # Finding the new w from del+E
+        self.w_T=self.w.T
+        print("Finished epoch:"+str(count))
 
     def scatterplotting(self,count):
         figure, axes = plt.subplots()
@@ -35,44 +45,41 @@ class Perceptron(object):
                 self.w_T=self.w.T
 
         x= np.linspace(-3,3,100)
-        axes.plot(x,-(self.w_T[0]/self.w_T[2])-(self.w_T[1]/self.w_T[2])*x, 'k')#plotting the line that linearly separates class 1 and 0
+        self.w_T=self.w.T
+        axes.plot(x,-(self.w_T[0]/self.w_T[2])-(self.w_T[1]/self.w_T[2])*x,'k')#plotting the line that linearly separates class 1 and 0
         plt.savefig('Scatterplots/foo'+str(count)+'.png')
+        plt.close()
         print("Finished plotting:"+str(count))
 
-    def epoch(self,count,learning_parameter):
-        for i in range(self.X.shape[0]):
-            wTx=np.dot(self.w_T,self.X[i])#Calculating W_transpose*x
-            if wTx*t[i]<0:#Finding those points incorrectly misclassified
-                self.E+=-1*wTx*self.t[i]#Calculating total error
-                self.del_E=X[i]*self.t[i]#Calculating delE
-                self.w=self.w-learning_parameter*self.del_E # Finding the new w from del+E
-        print("Finished epoch:"+str(count))
+    def compute(self):
+        for count in range(0,self.total_cycles):
+            self.epoch(count)
+            self.scatterplotting(count)
 
-    def estimate_learning(self):
+    def compute_noplot(self):
+        for count in range(0,self.total_cycles):
+            self.epoch(count)
+            # self.scatterplotting(count)
+
+class Testing:
+    def estimate_learning(self,total_cycles):
         lp_possibilities = np.geomspace(0.001,10,10)
-        total_cycles = 10
 
         for learning_parameter in lp_possibilities:
+            percep = Perceptron(X,t,total_cycles,learning_parameter)
+            percep.compute_noplot()
             figure, axes = plt.subplots()
-            # axes.plot(self.X, self.y)
+
+            x= np.linspace(0,100,100)
+            axes.plot(x,percep.E, 'k')
+
+
             axes.set_title("Learning Rate"+str(learning_parameter))
             axes.set_xlabel("Number of Iterations")
             axes.set_ylabel("Error")
 
-            for count in range(1,total_cycles+1):
-                percep.epoch(count,learning_parameter)
-                axes.plot(count,self.E,'bo')
-
             plt.savefig('Learning'+str(learning_parameter)+'.png')
-
-        print("Finished plotting:"+str(count))
-
-#plt.subplots(212)
-"""
-Final line is of the form w0+w1x1+w2x2=0
-The graph is plotted between x1 and x2
-"""
-
+            print("Finished estimating:"+str(learning_parameter)+"\n\n")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate a perceptron')
@@ -85,10 +92,10 @@ if __name__ == '__main__':
     X = dataset.iloc[:, 1:4].values
     y = dataset.iloc[:, -1].values
     t=np.where(y==0, -1, y)#Replaces class 0 to class -1
+    # print(t.shape[0])
     learning_parameter = 0.01
-    percep = Perceptron(X,y,t,learning_parameter)
-    for count in range(1,args.c+1):
-        percep.epoch(count,learning_parameter)
-        percep.scatterplotting(count)
+    percep = Perceptron(X,t,args.c,learning_parameter)
+    percep.compute()
 
-    # percep.estimate_learning()
+    # test = Testing()
+    # test.estimate_learning(100)
